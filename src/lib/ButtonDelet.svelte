1fbb185d9da6fdec70ecd0a94a20c34625aba117
arthurritzel <ritzelarthur@gmail.com>
2023-11-08 01:25:40 -0300

    deletar usuarios

---
+++ src/lib/ButtonDelet.svelte
@@ -0,0 +1,14 @@
+<script>
+    export var id = -1
+    export var table = ""
+    var dadosJSdelet = []
+    async function delet(){
+        var dados = await fetch("http://localhost:8080/"+table+"/excluir/"+id, {
+            method: "DELETE"
+        })
+        dadosJSdelet = await dados.json();
+    
+    }
+</script>
+
+<button class="bg-red-600 p-2 rounded-xl" on:click={delet}>Deletar</button>
--- src/routes/usuarios/+page.svelte
+++ src/routes/usuarios/+page.svelte
@@ -1,8 +1,10 @@
 <script>
-
-    console.log("teste")
+    import Api from "$lib/Lista.svelte"
+    import dadosJS from "$lib/Lista.svelte"
+    
     
 </script>
 
 <p class="text-center text-4xl py-9 underline decoration-sky-500">Gerenciar Usuarios</p>
+<Api url="usuarios"></Api>
 
